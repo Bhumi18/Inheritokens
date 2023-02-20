c4a296fd7370dc13608b32b0d2dc171f28b8b188
Sadariya Bhumi <bhumisadariya18@gmail.com>
2023-02-20 18:14:47 +0530

    changes

--- Inheritokens Frontend/hardhat.config.js
+++ Inheritokens Frontend/hardhat.config.js
@@ -1,14 +1,16 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 require("@nomicfoundation/hardhat-toolbox");
+
+const dotenv = require("dotenv");
+dotenv.config({ path: ".env" });
+
 module.exports = {
   solidity: "0.8.17",
   networks: {
     mumbai: {
       chainId: 80001,
       url: "https://polygon-mumbai.g.alchemy.com/v2/3ohLMSxKL3LyiT3ppE7-raO4PAOgbX-b",
-      accounts: [
-        "0xbc34a6b7f9d5c5277728b68b494bf4a095af70c2e46061e5a7ae30161db021a2",
-      ],
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
 };
